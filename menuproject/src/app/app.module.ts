4b2fad59bca5019e81d1f4765b1bcd422b211883
Unknown <celinemdemaziere@live.fr>
2018-09-24 16:10:03 +0200

    Création header-footer + centrage body

--- menuproject/src/app/app.module.ts
+++ menuproject/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './modules/ui/header/header.component';
+import { FooterComponent } from './modules/ui/footer/footer.component';
+import { LayoutComponent } from './modules/ui/layout/layout.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LayoutComponent
   ],
   imports: [
     BrowserModule
