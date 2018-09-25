5d4bc28e269259a7dc9b2901aa29dbcf6c3cc49f
BlackWidow <sandy.lawr@yahoo.fr>
2018-09-25 09:45:35 +0200

    Auto stash before merge of "dev" and "origin/dev"
    nouveau component : calendrier

--- menuproject/src/app/app.module.ts
+++ menuproject/src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './modules/ui/header/header.component';
 import { FooterComponent } from './modules/ui/footer/footer.component';
 import { LayoutComponent } from './modules/ui/layout/layout.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 
 @NgModule({
   declarations: [
@@ -12,6 +13,7 @@ import { LayoutComponent } from './modules/ui/layout/layout.component';
     HeaderComponent,
     FooterComponent,
     LayoutComponent
+    CalendarComponent
   ],
   imports: [
     BrowserModule
