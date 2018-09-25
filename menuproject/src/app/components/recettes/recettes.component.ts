d21a8173d6cd524e8b204783db6fa26595e5fae7
romainh <romainh.fr@gmail.com>
2018-09-25 14:33:03 +0200

    Ajout d'une liste de recettes sur 2 colonnes

--- menuproject/src/app/components/recettes/recettes.component.ts
+++ menuproject/src/app/components/recettes/recettes.component.ts
@@ -9,7 +9,36 @@ import { RecetteInterface } from './../../shared/interfaces/recette-interface';
 })
 export class RecettesComponent implements OnInit {
 
-  constructor(public ngxSmartModalService: NgxSmartModalService) { }
+  public recettes: RecetteInterface[];
+
+  constructor(public ngxSmartModalService: NgxSmartModalService) {
+    this.recettes = [
+      {
+        titre: 'Recette 1',
+        instructions: 'Faire ca',
+        tempsCuisson: 10,
+        tempsPreparation: 5,
+        nombrePersonnes: 2,
+        typeMeal: 1
+      },
+      {
+        titre: 'Recette 2',
+        instructions: 'Faire autre chose',
+        tempsCuisson: 4,
+        tempsPreparation: 50,
+        nombrePersonnes: 4,
+        typeMeal: 2
+      },
+      {
+        titre: 'Recette 3',
+        instructions: 'Plein de trucs',
+        tempsCuisson: 10,
+        tempsPreparation: 10,
+        nombrePersonnes: 3,
+        typeMeal: 3
+      }
+    ];
+  }
 
   ngOnInit() {
   }
