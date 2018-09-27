cd7fbb4c6f695d2f24028659227c33ecda74eff8
romainh <romainh.fr@gmail.com>
2018-09-27 09:40:51 +0200

    Cliquer sur + dans calendar ouvre la liste de toutes les recettes. Dans cette liste, appuyer sur détails, ouvre une autre popup avec les détails.

--- menuproject/src/app/components/ajoutrecette/ajoutrecette.component.ts
+++ menuproject/src/app/components/ajoutrecette/ajoutrecette.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { RecetteService } from './../../shared/services/recette-service.service';
+import { RecetteInterface } from './../../shared/interfaces/recette-interface';
+import { Subscription } from 'rxjs';
+import { TypePlat } from '../../shared/interfaces/type-plat';
+import { NgxSmartModalService } from 'ngx-smart-modal';
 
 @Component({
   selector: 'app-ajoutrecette',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AjoutrecetteComponent implements OnInit {
 
-  constructor() { }
+  /**
+   * @var recettes : Tableau de recette interface qui sont les données les recettes depuis la bdd
+   */
+  public recettes: RecetteInterface[];
+
+  /**
+   * @var recettesSorted: Tableau de recette interface qui sont les données triées par type de plat
+   */
+  public recettesSorted: RecetteInterface[];
+
+  /**
+   * Subscription au recettes depuis le service
+   */
+  private recettesSubscription: Subscription;
+
+  /**
+   * @var typeMeal : de type TypePlat qui contient id et libelle d'un type de plat
+   */
+  private typeMeal: TypePlat;
+
+  /**
+   * Subscription au type de plat depuis le service
+   */
+  private typePlatSubscription: Subscription;
+  // private typeMealLabel: String[];
+
+  constructor(public ngxSmartModalService: NgxSmartModalService, private recetteService: RecetteService) {
+    this.recettesSorted = [];
+    this.typeMeal = {
+      libelle: ''
+    };
+
+
+    // recupération des recettes
+    this.recettesSubscription = this.recetteService.getRecettes().subscribe((recettes) => {
+      // recupère les recettes de la bdd et les sauvegardes dans recettes
+      this.recettes = recettes;
+      // par défaut tri les recettes pour afficher les entrées
+      // this.sortRecette(1);
+    });
+
+    // récupération des types de plats
+    this.typePlatSubscription = this.recetteService.getTypePlat().subscribe((typePlat) => {
+      this.typeMeal = typePlat;
+      // enregistre chaque libelle dans le tableau de string typeMealLabel
+      // for (let i = 0; i < 3; i++ ) {
+      //  this.typeMealLabel.push(this.typeMeal[i].libelle);
+     // }
+     // console.log(' les labels de plat ' + this.typeMealLabel);
+    });
+  }
 
   ngOnInit() {
+  }
+
+  public detailId(id: number): void {
+    this.ngxSmartModalService.getModal('popupOne').open();
+    this.recetteService.sendId(id);
+    console.log('Id send is : ' + id);
   }
 
 }
